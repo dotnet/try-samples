c7283a6bf27e842af3f973457f5e39118de83c90
Bill Wagner <wiwagn@microsoft.com>
2019-08-29 08:47:49 -0400

    checkpoint - switching machines.

---
+++ 101-linq-samples/readme.md
@@ -0,0 +1,22 @@
+# 101 LINQ Samples
+![dotnet try Enabled](https://img.shields.io/badge/Try_.NET-Enabled-501078.svg)
+
+<p align ="center">
+<img src ="https://user-images.githubusercontent.com/2546640/56708992-deee8780-66ec-11e9-9991-eb85abb1d10a.png" width="350">
+</p>
+
+Learn how to use LINQ in your applications with these code samples, covering the entire range of LINQ functionality and demonstrating LINQ with objects, Entity Framework, and XML.
+
+## Lessons in this tutorial
+
+These lessons are organized according to the operations used:
+
+### Restriction operators
+
+These operators restrict, or filter, the input sequence to produce an output sequence.
+
+- Where - Simple 1
+- Where - Simple 2
+- Where - Simple 3
+- Where - Drilldown
+- Where - Indexed
---
+++ 101-linq-samples/src/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Try101LinqSamples
+{
+    class Program
+    {
+        ///<param name="region">Takes in the --region option from the code fence options in markdown</param>
+        ///<param name="session">Takes in the --session option from the code fence options in markdown</param>
+        ///<param name="package">Takes in the --package option from the code fence options in markdown</param>
+        ///<param name="project">Takes in the --project option from the code fence options in markdown</param>
+        ///<param name="args">Takes in any additional arguments passed in the code fence options in markdown</param>
+        ///<see>To learn more see <a href="https://aka.ms/learntdn">our documentation</a></see>
+        static int Main(
+            string region = null,
+            string session = null,
+            string package = null,
+            string project = null,
+            string[] args = null)
+        {
+
+            new Restrictions().LowNumbers();
+            new Restrictions().ProductsOutOfStock();
+            return 0;
+        }
+    }
+}
