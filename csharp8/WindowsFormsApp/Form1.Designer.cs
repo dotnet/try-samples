a6849c6043ca815744703fcb872a404dcf98bddb
Yi HAN <hanyi@8110@gmail.com>
2020-03-31 16:57:01 +0200

    Another way to change UI after async operation

--- csharp8/WindowsFormsApp/Form1.Designer.cs
+++ csharp8/WindowsFormsApp/Form1.Designer.cs
@@ -58,6 +58,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
--- csharp8/WindowsFormsApp/Form1.cs
+++ csharp8/WindowsFormsApp/Form1.cs
@@ -21,7 +21,9 @@ namespace WindowsFormsApp
         private async void button1_Click(object sender, EventArgs e)
         {
             Debug.WriteLine($"Main: {ThreadId()}");
-            var hash = await DownloadGoogle().ConfigureAwait(true);
+            var hash = await DownloadGoogle()
+                             .ConfigureAwait(true); // This is default behaviour
+                                                                         // Equivalent to:  var hash = await DownloadGoogle()
 
             Debug.WriteLine($"Write hash: {ThreadId()}");
             this.textBox1.Text = hash;
@@ -32,10 +34,23 @@ namespace WindowsFormsApp
             var uri = new Uri("http://www.google.com");
             var client = new HttpClient { BaseAddress = uri };
 
+            Debug.WriteLine($"Before download: {ThreadId()}");
+
             var downloadGoogle = (await client.GetAsync(uri)).GetHashCode().ToString();
 
             Debug.WriteLine($"After download: {ThreadId()}");
             return downloadGoogle;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"Main: {ThreadId()}");
+            await DownloadGoogle().ContinueWith(async (Task<string> task, object state) =>
+            {
+                var hash = await task;
+                Debug.WriteLine($"Write hash: {ThreadId()}");
+                this.textBox1.Text = hash;
+            }, null, TaskScheduler.FromCurrentSynchronizationContext()); 
         }
     }
 }
